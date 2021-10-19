7ccc742d027afccdcd1c317763463ca2d8364e87
piro <piro.haniwa@gmail.com>
2021-10-19 10:43:07 +0900

    feat: initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import { GetServerSideProps } from "next";
+
+export type PagesProps = {
+  hoge: string;
+};
+
+function Pages({ hoge }: PagesProps): JSX.Element {
+  return <div>{hoge}</div>;
+}
+
+export const getServerSideProps: GetServerSideProps<PagesProps> = async () => {
+  return {
+    props: {
+      hoge: "hogehoge",
+    },
+  };
+};
+
+export default Pages;
